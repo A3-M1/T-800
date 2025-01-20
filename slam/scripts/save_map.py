fe81af712d6b307897fa1791af9ff5cbe29cf5d3
Geoff <ounas35@gmail.com>
2025-01-20 17:33:17 +0100

    map

--- slam/scripts/save_map.py
+++ slam/scripts/save_map.py
@@ -2,7 +2,11 @@
 
 import rclpy # type: ignore
 from rclpy.node import Node # type: ignore
-from nav_msgs.msg import OccupancyGrid
+#from nav_msgs.msg import OccupancyGrid
+from nav_msgs.msg import MapMetaData
+from tf2_msgs.msg import TFMessage
+from geometry_msgs.msg import PoseWithCovarianceStamped
+
 
 # Start the node
 def startNode():
@@ -17,21 +21,40 @@ def startNode():
 
 # Node execution
 class ROSmap():
+
     def __init__(self, rosNode):
         # Logger
         self._logger = rosNode.get_logger()
 
+
         # Subscribers to events that can trigger the emergency stop
-        self._sub_map = rosNode.create_subscription(OccupancyGrid, '/map',self.map_callback, 10)
+        #self._sub_map = rosNode.create_subscription(OccupancyGrid, '/map',self.map_callback, 10)
+        self._sub_map_metadata = rosNode.create_subscription(MapMetaData, '/map_metadata',self.map_metadata_callback, 10)
+        #self._sub_tf= rosNode.create_subscription(TFMessage, '/tf',self.tf_callback, 10)
+        self._sub_tf= rosNode.create_subscription(PoseWithCovarianceStamped, '/pose',self.pose_callback, 10)
 
         # Log the start
         self._logger.info('Started !')
-    
-    
-    # Button callback
-    def button_callback(self, msg):
 
-        self._logger.info('newmap')
+
+    # Map callback
+    #def map_callback(self, msg):
+        #self._logger.info(msg.data)
+
+    # Map_metadata callback
+    def map_metadata_callback(self, msg):
+        self._logger.info(f"Resolution: {msg.resolution}")
+        self._logger.info(f"Width: {msg.width}")
+        self._logger.info(f"Height: {msg.height}")
+      
+    # Tf callback
+    #def tf_callback(self, msg):
+    #   self._logger.info(f"Transform: {msg.transforms}")
+    
+    # Tf callback
+    def pose_callback(self, msg):
+        self._logger.info(f"Pose: {msg.pose}")
+
 
 # Execute the function.
 if __name__ == "__main__":
