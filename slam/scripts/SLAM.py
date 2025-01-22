ff2b95791433fc2b1633a5d79b859d6870409021
Geoff <ounas35@gmail.com>
2025-01-22 12:03:11 +0100

    maps

---
+++ slam/scripts/SLAM.py
@@ -0,0 +1,72 @@
+#!/usr/bin/python3
+
+import numpy as np
+import rclpy # type: ignore
+from rclpy.node import Node # type: ignore
+from nav_msgs.msg import OccupancyGrid
+from nav_msgs.msg import MapMetaData
+import random as rd
+
+# Start the node
+def startNode():
+    rclpy.init()
+    aNode= Node("slam_map")
+    map = ROSmap(aNode)
+    # Start infinite loop
+    rclpy.spin(aNode)
+    # Clean everything and switch the light off
+    aNode.destroy_node()
+    rclpy.shutdown()
+
+# Node execution
+class ROSmap():
+
+    def __init__(self, rosNode):
+        # Logger
+        self._logger = rosNode.get_logger()
+        self._sub_map = rosNode.create_subscription(OccupancyGrid, '/map',self.map_callback, 10)
+        # Log the start
+        self._logger.info('Started !')
+
+    # Map callback
+    def map_callback(self, msg):
+        # 100 = mur
+        # -1 = jsp
+        # 0 = libre
+        # self._logger.info(msg.data)
+        slam_map = np.reshape(msg.data, (msg.info.width, msg.info.height ))
+        # self._logger.info(str(slam_map))
+        # self.point_aleatoire(msg)
+
+    #def point_aleatoire(self, msg):
+    #    slam_map = np.reshape(msg.data, (msg.info.width, msg.info.height ))
+    #    tab_points = [] # chart for the points unexplored by the robot
+    #    # addition of the points unexplored
+    #    for x in range (len(slam_map)):
+    #        for y in range (len(slam_map[x])):
+    #            if slam_map[x][y] == -1 :
+    #                tab_points.append((x,y))
+    #    # random selection of a point unexplored
+    #    point = tab_points[rd.randint(0, len(tab_points)-1)]
+
+    #    self._logger.info(str(point))
+
+    def gros_pixel(self,msg):
+        somme_gros_pixel = 0
+        slam_map = np.reshape(msg.data, (msg.info.width, msg.info.height ))
+        for i in range (len(slam_map) - 10):
+            for j in range (len(slam_map) - 10) :
+                gros_pixel = slam_map[i : i+10][j : j+10]
+                if np.sum(gros_pixel) < 0 and np.sum(gros_pixel) > -100 and np.sum(gros_pixel) < somme_gros_pixel :
+                    somme_gros_pixel = np.sum(gros_pixel)  
+                    best_i = i + 5
+                    best_j = j + 5
+    return(best_i, best_j)  
+
+
+
+
+
+# Execute the function.
+if __name__ == "__main__":
+    startNode()
--- slam/scripts/data_map.py
+++ slam/scripts/data_map.py
@@ -3,14 +3,13 @@
 # position par rapport à /map 
 # il va vers la position en contournant l'obstacle 
 # 
-
+import math
 import numpy as np
 import rclpy # type: ignore
 from rclpy.node import Node # type: ignore
 from nav_msgs.msg import OccupancyGrid
 from nav_msgs.msg import MapMetaData
 from tf2_msgs.msg import TFMessage
-from geometry_msgs.msg import PoseWithCovarianceStamped, TransformStamped, Pose, Transform, Vector3, Point, Quaternion
 from geometry_msgs.msg import PoseWithCovarianceStamped, TransformStamped, Pose, Transform, Vector3, Point, Quaternion
 
 # Start the node
@@ -24,8 +23,6 @@ def startNode():
     aNode.destroy_node()
     rclpy.shutdown()
 
-
-
 # Node execution
 class ROSmap():
 
@@ -35,9 +32,10 @@ class ROSmap():
         # Subscribers to events that can trigger the emergency stop
         #self._sub_map = rosNode.create_subscription(OccupancyGrid, '/map',self.map_callback, 10)
         #self._sub_map_metadata = rosNode.create_subscription(MapMetaData, '/map_metadata',self.map_metadata_callback, 10)
-        self._sub_tf= rosNode.create_subscription(TFMessage, '/tf',self.tf_callback, 10)
-        #self._sub_tf= rosNode.create_subscription(PoseWithCovarianceStamped, '/pose',self.pose_callback, 10)
-
+        #self._sub_tf= rosNode.create_subscription(TFMessage, '/tf',self.tf_callback, 10)
+        self._sub_tf= rosNode.create_subscription(PoseWithCovarianceStamped, '/pose',self.pose_callback, 10)
+        self._angle = 0
+        self._distance = 0
         # Log the start
         self._logger.info('Started !')
 
@@ -47,7 +45,8 @@ class ROSmap():
         # -1 = jsp
         # 0 = libre
         #self._logger.info(msg.data)
-        #self._logger.info(str(np.reshape(msg.data, (msg.info.width, msg.info.height ))))
+        #tab_map = np.reshape(msg.data, (msg.info.width, msg.info.height )
+        #self._logger.info(str(tab_map)))
 
 
     # Map_metadata callback
@@ -57,23 +56,29 @@ class ROSmap():
     #    self._logger.info(f"Height: {msg.height}")
 
     # Tf callback
-    def tf_callback(self, msg):
-        if len(msg.transforms) > 0: 
-            transform = msg.transforms[0]  
-            translation = transform.transform.translation  
-            self._logger.info(f"Transform translation: x={translation.x}, y={translation.y}, z={translation.z}")
-            rotation = transform.transform.rotation 
-            self._logger.info(f"Transform rotation: x={rotation.x}, y={rotation.y}, z={rotation.z}, w={rotation.w}")
- 
+    #def tf_callback(self, msg):
+    #    if len(msg.transforms) > 0: 
+    #        transform = msg.transforms[0]  
+     #       translation = transform.transform.translation  
+     #       #self._logger.info(f"Transform translation: x={translation.x}, y={translation.y}, z={translation.z}")
+     #       rotation = transform.transform.rotation 
+     #       #self._logger.info(f"Transform rotation: x={rotation.x}, y={rotation.y}, z={rotation.z}, w={rotation.w}")
+     #       self._angle = math.atan2(translation.y, translation.x)
+     #       self._distance = math.sqrt(translation.x**2 + translation.y**2)
+
+        #if self._angle != -2.163802305531382 and self._distance != 0.024475186458227053:
+        #self._logger.info(f"angle={self._angle*57,2958}, distance={self._distance}")
+
         #self._logger.info(f"Transform: {msg.transforms}")
 
     # Pose callback
-    #def pose_callback(self, msg):
+    def pose_callback(self, msg):
     #    # Access the position
-    #    position = msg.pose.pose.position
-    #    self._logger.info(f"Position: x={position.x}, y={position.y}, z={position.z}")
-    #    orientation = msg.pose.pose.orientation
-    #    self._logger.info(f"Orientation: x={orientation.x}, y={orientation.y}, z={orientation.z}, w={orientation.w}")
+        position = msg.pose.pose.position
+        self._logger.info("ffpiçsqdhfpisqdrfpsd")
+        self._logger.info(f"Position: x={position.x}, y={position.y}, z={position.z}")
+        orientation = msg.pose.pose.orientation
+        self._logger.info(f"Orientation: x={orientation.x}, y={orientation.y}, z={orientation.z}, w={orientation.w}")
 
 # Execute the function.
 if __name__ == "__main__":
